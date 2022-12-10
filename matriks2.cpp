cc5a67ae4414af03fa9f1d560a133106d64d7406
Rafif Ramadan <ramadanrafif30@gmail.com>
2022-12-10 17:19:05 +0700

    Program jadi sementara untuk tugas matriks

--- matriks2.cpp
+++ matriks2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     // Deklarasi variabel
-    int matriks1[10][10], matriks2[10][10], hasil[10][10], hasil2[10][10];
+    int matriks1[100][100], matriks2[100][100], hasil[100][100], hasil2[100][100];
     int pilihan, baris, kolom;
 
     // Input jumlah baris dan kolom
@@ -14,25 +14,25 @@ int main()
     cin >> kolom;
 
     // Input elemen matriks pertama
-    cout << "Masukkan elemen matriks pertama: " << endl;
+    cout << "\n|===Masukkan elemen matriks pertama===|" << endl;
     for (int i = 0; i < baris; i++)
     {
-        cout << "Masukkan element for baris ke-" << i+1 << ": ";
+        cout << "Masukkan element baris dan kolom ke-" << i + 1 << ": \n";
         for (int j = 0; j < kolom; j++)
         {
-            //cout << "Masukkan Elemen Matriks A [" << i << "][" << j << "] : ";
+            cout << "Masukkan Elemen Matriks A [" << i << "][" << j << "] : ";
             cin >> matriks1[i][j];
         }
     }
 
     // Input elemen matriks kedua
-    cout << "Masukkan elemen matriks kedua: " << endl;
+    cout << "\n|===Masukkan elemen matriks kedua===|" << endl;
     for (int i = 0; i < baris; i++)
     {
-        cout << "Masukkan element for baris ke-" << i+1 << ": ";
+        cout << "Masukkan element baris dan kolom ke-" << i + 1 << "\n";
         for (int j = 0; j < kolom; j++)
         {
-            //cout << "Masukkan Elemen Matriks A [" << i << "][" << j << "] : ";
+            cout << "Masukkan Elemen Matriks B [" << i << "][" << j << "] : ";
             cin >> matriks2[i][j];
         }
     }
@@ -47,70 +47,127 @@ int main()
         cout << "5. Keluar" << endl;
         cout << "Pilihan [1-4] : ";
         cin >> pilihan;
-
+        system("cls");
         // Lakukan operasi sesuai pilihan user
         switch (pilihan)
         {
         case 1:
+            cout << "Input Matriks yang diinput tadi : " << endl;
+            for (int i = 0; i < baris; i++)
+            {
+                //cout << "[";
+                for (int j = 0; j < kolom; j++)
+                {
+                    cout << matriks1[i][j] << " ";
+                    cout << matriks2[i][j] << " ";
+                    cout << endl;
+                }
+                //cout << "]";
+                cout << endl;
+            }
             cout << "Hasil penambahan: " << endl;
             for (int i = 0; i < baris; i++)
             {
-                cout << "[ ";
+                //cout << "[ ";
                 for (int j = 0; j < kolom; j++)
                 {
                     hasil[i][j] = matriks1[i][j] + matriks2[i][j];
                     cout << hasil[i][j] << " ";
                 }
-                cout << "]\n";
+                cout << endl;
             }
             break;
 
         case 2:
+        cout << "Input Matriks yang diinput tadi : " << endl;
+            for (int i = 0; i < baris; i++)
+            {
+                //cout << "[";
+                for (int j = 0; j < kolom; j++)
+                {
+                    cout << matriks1[i][j] << " ";
+                    cout << matriks2[i][j] << " ";
+                    cout << endl;
+                }
+                //cout << "]";
+                cout << endl;
+            }
             cout << "Hasil pengurangan: " << endl;
             for (int i = 0; i < baris; i++)
             {
-                cout << "[ ";
+                //cout << "[ ";
                 for (int j = 0; j < kolom; j++)
                 {
                     hasil[i][j] = matriks1[i][j] - matriks2[i][j];
                     cout << hasil[i][j] << " ";
                 }
-                cout << "]\n";
+                //cout << "]\n";
             }
             break;
 
         case 3:
+        cout << "Input Matriks yang diinput tadi : " << endl;
+            for (int i = 0; i < baris; i++)
+            {
+                //cout << "[";
+                for (int j = 0; j < kolom; j++)
+                {
+                    cout << matriks1[i][j] << " ";
+                    cout << matriks2[i][j] << " ";
+                    cout << endl;
+                }
+                //cout << "]";
+                cout << endl;
+            }
             cout << "Hasil perkalian: " << endl;
             for (int i = 0; i < baris; i++)
             {
-                cout << "[ ";
+                //cout << "[ ";
                 for (int j = 0; j < kolom; j++)
                 {
                     hasil[i][j] = matriks1[i][j] * matriks2[i][j];
                     cout << hasil[i][j] << " ";
                 }
-                cout << "]\n";
+                //cout << "]\n";
             }
             break;
 
         case 4:
+        cout << "Input Matriks yang diinput tadi : " << endl;
+            for (int i = 0; i < baris; i++)
+            {
+                //cout << "[";
+                for (int j = 0; j < kolom; j++)
+                {
+                    cout << matriks1[i][j] << " ";
+                    cout << matriks2[i][j] << " ";
+                    cout << endl;
+                }
+                //cout << "]";
+                cout << endl;
+            }
             cout << "Hasil Transpose: " << endl;
             for (int i = 0; i < baris; i++)
             {
-                cout << "[ ";
+                //cout << "[ ";
                 for (int j = 0; j < kolom; j++)
                 {
                     hasil[i][j] = matriks1[j][i];
                     hasil2[i][j] = matriks2[j][i];
-                    cout << hasil[i][j] << " ";
-                    cout << hasil2[i][j] << " ";
+                    cout << hasil[i][j] << " "<<endl;
+                    cout << hasil2[i][j] << " "<<endl;
                 }
-                cout << "]\n";
+                //cout << "]\n";
             }
             break;
 
+        case 5:
+            cout << "Anda Keluar dari Program" << endl;
+            break;
+
         default:
-            cout << "Anda tidak memilih pilihan yang tersedia";
+            cout << "Pilihan tidak tersedia" << endl;
+            break;
         }
 
     } while (pilihan != 5);
